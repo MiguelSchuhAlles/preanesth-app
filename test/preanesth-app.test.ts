65642ef4b51af701b60d77bb755ba0ad2633d14d
Miguel Schuh Alles <msalles@amazon.com>
2025-09-28 23:01:24 -0300

    Initial commit

---
+++ test/preanesth-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PreanesthApp from '../lib/preanesth-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/preanesth-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PreanesthApp.PreanesthAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
